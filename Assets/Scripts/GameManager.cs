504f41fad2f5a683581b9d847c29bd8a99b04d6c
Nyrika <mateo.le_breton@edu.devinci.fr>
2022-10-20 12:54:29 +0200

    Game manager in progress

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour
 {
     public int Score = 0;
 
+    public float reloadSpawn;
+    private bool reloading;
+
     public GameObject PigLeftPrefab;
     public GameObject PigRightPrefab;
 
@@ -18,10 +21,27 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Score < 5) {
+            if (!reloading) {
+                Instantiate( PigLeftPrefab, new Vector3(-13,Random.Range(-3.5f, 2.5f),0), Quaternion.identity);
+            }
+        }
+
+        if (Score >= 5 && Score < 10) {
+
+        }
+
+        if (Score >= 10 && Score < 15) {
+
+        }
     }
 
     void PlusScore() {
         Score++;
+    }
+
+    IEnumerator waitSpawn() {
+        yield return new WaitForSeconds(reloadSpawn);
+        reloading = false;
     }
 }
